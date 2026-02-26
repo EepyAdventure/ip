cba5c6c580d51fb4070801022899fd6080d2c86e
EepyAdventure <joshua.poon181818@gmail.com>
2026-02-26 15:47:00 +0800

    Add automated testing

---
+++ src/main/java/Process/Action.java
@@ -0,0 +1,74 @@
+package Process;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Action {
+    private static Process process;
+    private static TaskList listTemp;
+    private static Path listPerm;
+    protected static boolean start(Process process_, TaskList listTemp_, Path listPerm_) {
+        process = process_;
+        listTemp = listTemp_;
+        listPerm = listPerm_;
+        return true;
+    }
+    protected static boolean exit() {
+        listTemp.clear();
+        return false;
+    }
+    protected static boolean echo(String... echo) {
+        System.out.printf(String.join(" ", echo) + "\n");
+        return true;
+    }
+    protected static boolean add(String taskType, String... description) throws Exception {
+        if (description.length == 0) {
+            throw new NukeException("You forgor the description :skull");
+        }
+        String[] fullDescription = new String[description.length + 1];
+        fullDescription[0] = "false";
+        System.arraycopy(description, 0, fullDescription, 1, description.length);
+        Task task = Task.makeTask(taskType, fullDescription);
+        listTemp.add(task);
+        System.out.printf("Task Added to list\n");
+        System.out.printf("  %s\n", task.toString());
+        System.out.printf("Now you have %d tasks in the list\n", listTemp.getCount());
+        return true;
+    }
+    protected static boolean delete(String index) {
+        Task task = listTemp.get(Integer.valueOf(index) - 1);
+        listTemp.remove(Integer.valueOf(index) - 1);
+        System.out.printf("I dragged them out the back\n");
+        System.out.printf("  %s\n", task.toString());
+        System.out.printf("Now you have %d tasks in the list\n", listTemp.getCount());
+        return true;
+    }
+    protected static boolean mark(String index) {
+        listTemp.get(Integer.valueOf(index) - 1).setStatus(true);
+        return true;
+    }
+    protected static boolean unmark(String index) {
+        listTemp.get(Integer.valueOf(index) - 1).setStatus(false);
+        return true;
+    }
+    protected static boolean list() throws Exception {
+        System.out.printf(listTemp.toString());
+        return true;
+    }
+    protected static boolean save() {
+        try {
+            Files.write(listPerm, listTemp.toSave(),
+                    StandardOpenOption.CREATE);
+            System.out.println("You are filled with determination");
+        } catch (IOException e) {
+            throw new NukeException("Your SAVE file DOES NOT EXIST");
+        }
+        return true;
+    }
+}
